8b6dcaeee5d0b9fc063865a93804f47b69116993
andrew <andrew.khmaladze@gmail.com>
2025-11-04 04:14:51 -0500

    changed Apptest file in order to get test section when this jenkins job fails

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,4 +1,13 @@
-public void testGreeting() {
-    String result = App.getGreeting();
-    assertEquals("Wrong Message!", result);  // <-- intentionally wrong
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+    @Test
+    public void testGreeting() {
+        String result = App.getGreeting();
+        assertEquals("Wrong Message!", result);  // <-- intentionally wrong
+    }
 }
